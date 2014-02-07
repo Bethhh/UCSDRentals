b91a97fb393e8a570fe1e15f7168919eb12ee754
vagrant <vagrant@precise32.(none)>
2014-02-07 04:04:19 +0000

    home page + link to menu

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var add =  require('./routes/add');
+var menu = require('./routes/menu')
 // Example route
 // var user = require('./routes/user');
 
@@ -38,6 +39,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/add', add.addFriend);
+app.get('/menu', menu.view)
 // Example route
 // app.get('/users', user.list);
 
