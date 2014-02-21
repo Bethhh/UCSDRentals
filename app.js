0a961176fc9b59c4f7f03873a8123f090f74cf55
vagrant <vagrant@precise32.(none)>
2014-02-21 11:47:55 +0000

    test to figure out heroku

--- app.js
+++ app.js
@@ -16,17 +16,17 @@ var about = require('./routes/about');
 var newProfile = require('./routes/newProfile');
 var matches = require('./routes/matches');
 var existing = require('./routes/existing');
-var types = require('./routes/types');
+/*var types = require('./routes/types');
 var properties = require('./routes/properties');
 var rent = require('./routes/rent');
 var address = require('./routes/address');
 var date = require('./routes/date');
 var other = require('./routes/other');
-var tmp = require('./routes/tmp');
+var tmp = require('./routes/tmp');*/
 
 var submit_type = require('./routes/submit_type');
-var oneProfile = require('./routes/oneProfile');
-var writeTypes = require('./routes/writeTypes');
+//var oneProfile = require('./routes/oneProfile');
+//var writeTypes = require('./routes/writeTypes');
 var user = require('./routes/user');
 // Example route
 // var user = require('./routes/user');
@@ -61,16 +61,16 @@ app.get('/about', about.view);
 app.get('/newProfile', newProfile.view);
 app.get('/matches', matches.view);
 app.get('/existing', existing.view);
-app.get('/types', types.viewForm);
+/*app.get('/types', types.viewForm);
 app.get('/properties', properties.viewForm);
 app.get('/rent', rent.viewForm);
 app.get('/address', address.viewForm);
 app.get('/date', date.viewForm);
-app.get('/other', other.viewForm);
-app.get('/tmp',tmp.view);
+app.get('/other', other.viewForm);*/
+//app.get('/tmp',tmp.view);
 //app.get('/submit_type', submit_type.submiWholeForm);
-app.get('/oneProfile', oneProfile.getForm);
-app.get('/writeTypes',writeTypes.write);
+//app.get('/oneProfile', oneProfile.getForm);
+//app.get('/writeTypes',writeTypes.write);
 
 app.get('/user', user.userInfo);
 app.post('/submit_type/:name/save', submit_type.save);
